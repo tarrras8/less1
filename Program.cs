24b06ea5df2646521f013b8d69b8eaa4ec1d6f40
tarrras8 <140727620+tarrras8@users.noreply.github.com>
2023-08-08 14:35:30 +0300

    задача2

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Введите число");
-int numberA = Convert.ToInt32(Console.ReadLine());  
-Console.WriteLine("Введите второе число");
-int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberA > numberB)
-Console.WriteLine("numberA");
-else Console.WriteLine("numberB");
